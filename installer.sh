fbc235252488a2dd4f299ded51a4da00d0dba68d
Minege <minege02@gmail.com>
2017-06-15 23:12:20 +0200

    added files

---
+++ installer.sh
@@ -0,0 +1,6 @@
+sudo pacman -S git playerctl keepassx compton firefox feh compton termite synapse
+unzip "font-awesome-4.7.0.zip"
+mv font-awesome-4.7.0/fonts/fontawesome-webfont.ttf /usr/share/fonts/TTF
+mv i3config ~/.config/i3/config
+mv termiteconfig ~/.config/termite/config
+rm -r font-awesome-4.7.0.zip font-awesome-4.7.0
