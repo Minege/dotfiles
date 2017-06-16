20766d780d019ca64f676465a964757b358841f3
Minege <minege02@gmail.com>
2017-06-16 19:37:12 +0200

    updated

--- installer.sh
+++ installer.sh
@@ -1,8 +1,9 @@
-sudo pacman -S git playerctl keepassx compton firefox feh compton termite synapse
+#!/bin/bash
 unzip "font-awesome-4.7.0.zip"
 mv font-awesome-4.7.0/fonts/fontawesome-webfont.ttf /usr/share/fonts/TTF
-rm $HOME/.config/i3/config
-rm $HOME/.config/termite/config
-mv i3config ~/.config/i3/config
-mv termiteconfig ~/.config/termite/config
+rm ~/.config/i3/config
+rm ~/.config/termite/config
+cp i3config ~/.config/i3/config
+mkdir ~/.config/termite/
+cp termiteconfig ~/.config/termite/config
 rm -r font-awesome-4.7.0.zip font-awesome-4.7.0
---
+++ require.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo pacman -S git playerctl keepassx compton firefox feh compton termite synapse
+sudo chown -R loan /usr/share/fonts
