9fc5b9b80123d74cb126aa6b02243348eba55e8f
Minege <Minege@users.noreply.github.com>
2017-07-05 12:56:06 +0200

    Update require.sh

--- require.sh
+++ require.sh
@@ -7,6 +7,3 @@ unzip "font-awesome-4.7.0.zip"
 mv "font-awesome-4.7.0/fonts/fontawesome-webfont.ttf" /usr/share/fonts/TTF
 rm "font-awesome-4.7.0.zip"
 rm -r "font-awesome-4.7.0"
-rm ~/.config/i3/config # Delete existing files
-rm ~/.config/i3/i3blocks.conf
-rm ~/.config/i3/scripts
