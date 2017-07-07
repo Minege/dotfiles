5e7cd7ad2cb417e78d6f14b8147301e27e0912ca
Loan <minege02@gmail.com>
2017-07-07 15:55:27 +0200

    add neovim

--- require.sh
+++ require.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo pacman -S sysstat rofi git playerctl keepassx2 compton firefox feh i3blocks lxappearance vlc arc-gtk-theme wget lxterminal gcc cmake python2 python3 rust cargo nodejs npm
+sudo pacman -S sysstat rofi git playerctl keepassx2 compton firefox feh i3blocks lxappearance vlc arc-gtk-theme wget lxterminal gcc cmake python2 python3 rust cargo nodejs npm neovim
 wget http://fontawesome.io/assets/font-awesome-4.7.0.zip
 unzip "font-awesome-4.7.0.zip"
 mv "font-awesome-4.7.0/fonts/fontawesome-webfont.ttf" /usr/share/fonts/TTF
