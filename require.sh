10cbd7c5330b34de5f59c8d21c5843b3f655acc5
Minege <minege02@gmail.com>
2017-07-09 05:43:53 +0200

    Update dotfiles

--- require.sh
+++ require.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-sudo pacman --needed -S sysstat rofi git playerctl keepassx2 compton firefox feh i3blocks lxappearance vlc arc-gtk-theme wget lxterminal gcc cmake python2 python3 rust cargo nodejs npm neovim
+sudo pacman --needed -S sysstat rofi git playerctl keepassx2 compton firefox feh i3blocks lxappearance vlc arc-gtk-theme wget lxterminal gcc cmake python2 python3 rust cargo nodejs npm neovim python-pip
 wget http://fontawesome.io/assets/font-awesome-4.7.0.zip
 unzip "font-awesome-4.7.0.zip"
 mv "font-awesome-4.7.0/fonts/fontawesome-webfont.ttf" /usr/share/fonts/TTF
 rm "font-awesome-4.7.0.zip"
 rm -r "font-awesome-4.7.0"
 pip3 install neovim
-echo 'Use lxappearance to modify the gtk theme'
+echo '\033[0;31mUse lxappearance to modify the gtk theme'
+ln -s /bin/nvim /usr/local/bin/vim  # Make sym. link to nvim ( vim = nvim )
+sleep 2
