2e3e8dd5ab52dda4b538eef4f7dd6d818adae836
Minege <minege02@gmail.com>
2017-07-16 00:58:55 +0200

    update

--- require.sh
+++ require.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-sudo apt-get install sysstat rofi git playerctl keepassx2 compton iceweasel feh i3blocks lxappearance vlc wget lxterminal gcc cmake python2 python3 python3-pip neovim python-pip acpi
+sudo apt-get update
+sudo apt-get upgrade 
+sudo apt-get install sysstat rofi git keepassx compton iceweasel feh i3blocks lxappearance vlc wget lxterminal gcc cmake python2 python3 python-pip python3-pip neovim acpi xbacklight
+
+#  ---- config for using inte brightness with xbacklight ----
+X11="/etc/X11/xorg.conf"
+echo 'Section "Device"' >> $X11
+echo '  Identifier  "Card0"' >> $X11
+echo '  Driver      "intel"' >> $X11
+echo '  Option      "Backlight"  "intel_backlight"' >> $X11
+echo 'EndSection' >> $X11
+
 # nodejs and npm
 wget http://fontawesome.io/assets/font-awesome-4.7.0.zip
 unzip "font-awesome-4.7.0.zip"
@@ -9,4 +20,4 @@ rm -r "font-awesome-4.7.0"
 pip3 install neovim
 echo '\033[0;31mUse lxappearance to modify the gtk theme'
 ln -s /bin/nvim /usr/local/bin/vim  # Make sym. link to nvim ( vim = nvim )
-echo 'Finish'
+echo '**~~  Finish  ~~**'
