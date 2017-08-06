5ca5298baf127b44414e69b37903159992bb93b3
Minege <Minege@users.noreply.github.com>
2017-08-06 15:35:54 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -8,13 +8,7 @@ mv i3/config ~/.config/i3/config
 mv i3/i3blocks.conf ~/.config/i3/i3blocks.conf
 mv i3/scripts ~/.config/i3/
 
-mkdir ~/.config/nvim
-mkdir ~/.config/nvim/bundle
-mv init.vim ~/.config/nvim
 
-git clone https://github.com/VundleVim/Vundle.vim ~/.config/nvim/bundle/Vundle.vim
-nvim +PluginInstall +qall
-~/.config/nvim/bundle/YouCompleteMe/install.py --racer-completer --clang-completer
 # wget https://download.jetbrains.com/python/pycharm-community-2017.1.4.tar.gz
 # tar -xf pycharm-community-*
 # rm pycharm-community-*.tar.gz
